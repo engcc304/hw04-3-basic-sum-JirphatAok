f04738674e0d98fe9734c4b940cf7259866709f9
Jirphat Saetae <jirphat.aok@gmail.com>
2023-08-05 16:27:38 +0700

    hw

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -18,8 +18,8 @@ int main() {
     int Num2 ;
     int Num3 ;
     int Answer ;
-    printf("Test case:" ) ;
     scanf( "%d %d %d" ,&Num1,&Num2,&Num3) ;
     Answer = Num1 + Num2 + Num3 ;
 printf( "Answer = %d",Answer ) ;
 return 0 ;
+}//endfunction
